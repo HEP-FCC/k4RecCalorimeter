792b1fe9717ea5b18b588fc24918464aacd7a2a1
Valentin Volkl <valentin.volkl@cern.ch>
2021-10-19 16:53:05 +0200

    CreateCaloCellsNoise: duplicate to have CalorimeterHit->CalorimeterHit processing
    since CreateCaloCells now takes SimCalorimeterHits as input, but this algo is still used to add noise to CalorimeterHits. Should be refactored/templated in the future.

---
+++ RecCalorimeter/src/components/CreateCaloCellsNoise.cpp
@@ -0,0 +1,131 @@
+#include "CreateCaloCellsNoise.h"
+
+// FCCSW
+#include "DetCommon/DetUtils.h"
+#include "k4Interface/IGeoSvc.h"
+
+// DD4hep
+#include "DD4hep/Detector.h"
+#include "DD4hep/Volumes.h"
+#include "TGeoManager.h"
+
+// EDM4HEP
+#include "edm4hep/CalorimeterHit.h"
+
+DECLARE_COMPONENT(CreateCaloCellsNoise)
+
+CreateCaloCellsNoise::CreateCaloCellsNoise(const std::string& name, ISvcLocator* svcLoc) :
+GaudiAlgorithm(name, svcLoc), m_geoSvc("GeoSvc", name) {
+  declareProperty("hits", m_hits, "Hits from which to create cells (input)");
+  declareProperty("cells", m_cells, "The created calorimeter cells (output)");
+
+  declareProperty("calibTool", m_calibTool, "Handle for tool to calibrate Geant4 energy to EM scale tool");
+  declareProperty("noiseTool", m_noiseTool, "Handle for the calorimeter cells noise tool");
+  declareProperty("geometryTool", m_geoTool, "Handle for the geometry tool");
+}
+
+StatusCode CreateCaloCellsNoise::initialize() {
+  StatusCode sc = GaudiAlgorithm::initialize();
+  if (sc.isFailure()) return sc;
+
+  info() << "CreateCaloCellsNoise initialized" << endmsg;
+  info() << "do calibration : " << m_doCellCalibration << endmsg;
+  info() << "add cell noise      : " << m_addCellNoise << endmsg;
+  info() << "remove noise cells below threshold : " << m_filterCellNoise << endmsg;
+  info() << "add position information to the cell : " << m_addPosition << endmsg;
+
+  // Initialization of tools
+  // Calibrate Geant4 energy to EM scale tool
+  if (m_doCellCalibration) {
+    if (!m_calibTool.retrieve()) {
+      error() << "Unable to retrieve the calo cells calibration tool!!!" << endmsg;
+      return StatusCode::FAILURE;
+    }
+  }
+  // Cell noise tool
+  if (m_addCellNoise) {
+    if (!m_noiseTool.retrieve()) {
+      error() << "Unable to retrieve the calo cells noise tool!!!" << endmsg;
+      return StatusCode::FAILURE;
+    }
+    // Geometry settings
+    if (!m_geoTool.retrieve()) {
+      error() << "Unable to retrieve the geometry tool!!!" << endmsg;
+      return StatusCode::FAILURE;
+    }
+    // Prepare map of all existing cells in calorimeter to add noise to all
+    StatusCode sc_prepareCells = m_geoTool->prepareEmptyCells(m_cellsMap);
+    if (sc_prepareCells.isFailure()) {
+      error() << "Unable to create empty cells!" << endmsg;
+      return StatusCode::FAILURE;
+    }
+  }
+  if (m_addPosition){
+    m_volman = m_geoSvc->lcdd()->volumeManager();
+  }
+  return StatusCode::SUCCESS;
+}
+
+StatusCode CreateCaloCellsNoise::execute() {
+  // Get the input collection with Geant4 hits
+  const edm4hep::CalorimeterHitCollection* hits = m_hits.get();
+  debug() << "Input Hit collection size: " << hits->size() << endmsg;
+
+  // 0. Clear all cells
+  if (m_addCellNoise) {
+    std::for_each(m_cellsMap.begin(), m_cellsMap.end(), [](std::pair<const uint64_t, double>& p) { p.second = 0; });
+  } else {
+    m_cellsMap.clear();
+  }
+
+  // 1. Merge energy deposits into cells
+  // If running with noise map already was prepared. Otherwise it is being
+  // created below
+  for (const auto& hit : *hits) {
+    verbose() << "CellID : " << hit.getCellID() << endmsg;
+    m_cellsMap[hit.getCellID()] += hit.getEnergy();
+  }
+  debug() << "Number of calorimeter cells after merging of hits: " << m_cellsMap.size() << endmsg;
+
+  // 2. Calibrate simulation energy to EM scale
+  if (m_doCellCalibration) {
+    m_calibTool->calibrate(m_cellsMap);
+  }
+
+  // 3. Add noise to all cells
+  if (m_addCellNoise) {
+    m_noiseTool->addRandomCellNoise(m_cellsMap);
+    if (m_filterCellNoise) {
+      m_noiseTool->filterCellNoise(m_cellsMap);
+    }
+  }
+
+  // 4. Copy information to CaloHitCollection
+  edm4hep::CalorimeterHitCollection* edmCellsCollection = new edm4hep::CalorimeterHitCollection();
+  for (const auto& cell : m_cellsMap) {
+    if (m_addCellNoise || (!m_addCellNoise && cell.second != 0)) {
+      edm4hep::CalorimeterHit newCell = edm4hep::CalorimeterHit();
+      newCell.setEnergy(cell.second);
+      uint64_t cellid = cell.first;
+      newCell.setCellID(cellid);
+      if (m_addPosition){
+        auto detelement = m_volman.lookupDetElement(cellid);
+        const auto& transformMatrix = detelement.nominal().worldTransformation();
+        double outGlobal[3];
+        double inLocal[] = {0, 0, 0};
+        transformMatrix.LocalToMaster(inLocal, outGlobal);
+        edm4hep::Vector3f position = edm4hep::Vector3f(outGlobal[0] / dd4hep::mm, outGlobal[1] / dd4hep::mm, outGlobal[2] / dd4hep::mm);
+        newCell.setPosition(position);
+      }
+      edmCellsCollection->push_back(newCell);
+    }
+  }
+
+  // push the CaloHitCollection to event store
+  m_cells.put(edmCellsCollection);
+  debug() << "Output Cell collection size: " << edmCellsCollection->size() << endmsg;
+
+  return StatusCode::SUCCESS;
+}
+
+StatusCode CreateCaloCellsNoise::finalize() { return GaudiAlgorithm::finalize(); }
