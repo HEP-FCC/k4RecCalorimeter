88b00f4f0b0ac0acdbf6de0ffaf57cbcacea31c4
Alexis Maloizel <almaloiz@lxplus917.cern.ch>
2024-03-10 23:43:55 +0100

    Fixed based on GM comments

--- RecFCCeeCalorimeter/src/components/CorrectClusterBarycenters.cpp
+++ RecFCCeeCalorimeter/src/components/CorrectClusterBarycenters.cpp
@@ -36,38 +36,26 @@ StatusCode CorrectClusterBarycenters::initialize()
       return sc;
   }
 
-  // int energyStart = 0;
-  // int energyEnd = 0;
-  // if( m_energy == 0 ) {
-  //   energyStart = 0;
-  //   energyEnd = 1000;
-  // } else {
-  //   energyStart = 0;
-  //   energyEnd = 5. * m_energy;
-  // }
-
-  for (uint iSys = 0; iSys < m_systemId.size(); iSys++) {
-    // check if readouts exist
-    if (m_geoSvc->getDetector()->readouts().find(m_readoutName[iSys]) == m_geoSvc->getDetector()->readouts().end()) {
-      error() << "Readout <<" << m_readoutName[iSys] << ">> does not exist." << endmsg;
-      return StatusCode::FAILURE;
-    }
-    // retrieve PhiTheta segmentation
-    m_segmentationPhiTheta[m_systemId[iSys]] = dynamic_cast<dd4hep::DDSegmentation::FCCSWGridPhiTheta_k4geo*>(
-        m_geoSvc->getDetector()->readout(m_readoutName[iSys]).segmentation().segmentation());
-    m_segmentationMulti[m_systemId[iSys]] = dynamic_cast<dd4hep::DDSegmentation::MultiSegmentation*>(
-        m_geoSvc->getDetector()->readout(m_readoutName[iSys]).segmentation().segmentation());
-    if (m_segmentationPhiTheta[m_systemId[iSys]] == nullptr && m_segmentationMulti[m_systemId[iSys]] == nullptr) {
-      error() << "There is no phi-eta or multi- segmentation." << endmsg;
-      return StatusCode::FAILURE;
-    }
-    m_decoder.insert (std::make_pair(m_systemId[iSys], m_geoSvc->getDetector()->readout(m_readoutName[iSys]).idSpec().decoder()));
-  }
-  // Initialize random service
-  if (service("RndmGenSvc", m_randSvc).isFailure()) {
-    error() << "Couldn't get RndmGenSvc!!!!" << endmsg;
+  // check if readouts exist
+  if (m_geoSvc->getDetector()->readouts().find(m_readoutName) == m_geoSvc->getDetector()->readouts().end()) {
+    error() << "Readout <<" << m_readoutName << ">> does not exist." << endmsg;
     return StatusCode::FAILURE;
   }
+  // retrieve PhiTheta segmentation
+  m_segmentationPhiTheta = dynamic_cast<dd4hep::DDSegmentation::FCCSWGridPhiTheta_k4geo*>(
+      m_geoSvc->getDetector()->readout(m_readoutName).segmentation().segmentation());
+  // m_segmentationMulti = dynamic_cast<dd4hep::DDSegmentation::MultiSegmentation*>(
+  //     m_geoSvc->getDetector()->readout(m_readoutName).segmentation().segmentation());
+  if (m_segmentationPhiTheta == nullptr
+      // && m_segmentationMulti == nullptr
+      ) {
+    error() << "There is no phi-theta segmentation." << endmsg;
+    return StatusCode::FAILURE;
+  }
+
+  m_decoder = m_geoSvc->getDetector()->readout(m_readoutName).idSpec().decoder();
+  return StatusCode::SUCCESS;
+
 }
 
 StatusCode CorrectClusterBarycenters::finalize()
@@ -81,11 +69,11 @@ StatusCode CorrectClusterBarycenters::execute()
   const edm4hep::ClusterCollection *inClusters = m_inClusters.get();
   edm4hep::ClusterCollection *correctedClusters = m_correctedClusters.createAndPut();
 
-  uint systemId = m_systemId[0];
+  uint systemId = m_systemId;
   const dd4hep::DDSegmentation::FCCSWGridPhiTheta_k4geo *segmentation = nullptr;
-  if (m_segmentationPhiTheta[systemId] != nullptr)
+  if (m_segmentationPhiTheta != nullptr)
   {
-    segmentation = m_segmentationPhiTheta[systemId];
+    segmentation = m_segmentationPhiTheta;
   }
   for (const auto &cluster : *inClusters)
   {
@@ -103,7 +91,7 @@ StatusCode CorrectClusterBarycenters::execute()
     // Do everything only using the first defined calorimeter (default: Ecal barrel)
     double oldThetaId = -1;
     double oldPhiId = -1;
-    if (m_segmentationPhiTheta[systemId] != nullptr)
+    if (m_segmentationPhiTheta != nullptr)
     {
       oldThetaId = int(floor((oldTheta + 0.5 * segmentation->gridSizeTheta() - segmentation->offsetTheta()) / segmentation->gridSizeTheta()));
       oldPhiId = int(floor((oldPhi + 0.5 * segmentation->gridSizePhi() - segmentation->offsetPhi()) / segmentation->gridSizePhi()));
@@ -114,35 +102,26 @@ StatusCode CorrectClusterBarycenters::execute()
     newCluster.setPosition(cluster.getPosition());
     for (auto cell = cluster.hits_begin(); cell != cluster.hits_end(); cell++)
     {
-      if (m_segmentationMulti[systemId] != nullptr)
+      // if (m_segmentationMulti != nullptr)
+      // {
+      //   segmentation = dynamic_cast<const dd4hep::DDSegmentation::FCCSWGridPhiTheta_k4geo *>(&m_segmentationMulti->subsegmentation(cell->getCellID()));
+      //   oldThetaId = int(floor((oldTheta + 0.5 * segmentation->gridSizeTheta() - segmentation->offsetTheta()) / segmentation->gridSizeTheta()));
+      //   oldPhiId = int(floor((oldPhi + 0.5 * segmentation->gridSizePhi() - segmentation->offsetPhi()) / segmentation->gridSizePhi()));
+      // }
+      if (m_decoder->get(cell->getCellID(), "system") == systemId)
       {
-        segmentation = dynamic_cast<const dd4hep::DDSegmentation::FCCSWGridPhiTheta_k4geo *>(&m_segmentationMulti[systemId]->subsegmentation(cell->getCellID()));
-        oldThetaId = int(floor((oldTheta + 0.5 * segmentation->gridSizeTheta() - segmentation->offsetTheta()) / segmentation->gridSizeTheta()));
-        oldPhiId = int(floor((oldPhi + 0.5 * segmentation->gridSizePhi() - segmentation->offsetPhi()) / segmentation->gridSizePhi()));
-      }
-      if (m_decoder[systemId]->get(cell->getCellID(), "system") == systemId)
-      {
-        uint layerId = m_decoder[systemId]->get(cell->getCellID(), "layer");
+        uint layerId = m_decoder->get(cell->getCellID(), "layer");
         if (m_nPhiFinal[layerId] > 0 && m_nThetaFinal[layerId] > 0)
         {
-          uint thetaId = m_decoder[systemId]->get(cell->getCellID(), "theta");
-          uint phiId = m_decoder[systemId]->get(cell->getCellID(), "phi");
+          uint thetaId = m_decoder->get(cell->getCellID(), "theta");
+          uint phiId = m_decoder->get(cell->getCellID(), "phi");
           if (thetaId >= (oldThetaId - m_halfThetaFin[layerId]) && thetaId <= (oldThetaId + m_halfThetaFin[layerId]) &&
               phiId >= phiNeighbour((oldPhiId - m_halfPhiFin[layerId]), segmentation->phiBins()) && phiId <= phiNeighbour((oldPhiId + m_halfPhiFin[layerId]), segmentation->phiBins()))
           {
-            if (m_ellipseFinalCluster)
-            {
-              if (pow((thetaId - oldThetaId) / (m_nThetaFinal[layerId] / 2.), 2) + pow((phiId - oldPhiId) / (m_nPhiFinal[layerId] / 2.), 2) < 1)
-              {
-                newCluster.addToHits(*cell);
-                energy += cell->getEnergy();
-              }
-            }
-            else
-            {
-              newCluster.addToHits(*cell);
-              energy += cell->getEnergy();
-            }
+
+            newCluster.addToHits(*cell);
+            energy += cell->getEnergy();
+
           }
         }
       }
@@ -150,76 +129,67 @@ StatusCode CorrectClusterBarycenters::execute()
     newCluster.setEnergy(energy);
 
     // 1. Correct theta position with log-weighting
-    double sumEnFirstLayer = 0;
+
     // get current pseudorapidity
     std::vector<double> sumEnLayer;
-    std::vector<double> sumEnLayerSorted;
     std::vector<double> sumThetaLayer;
     std::vector<double> sumWeightLayer;
     sumEnLayer.assign(m_numLayers, 0);
-    sumEnLayerSorted.assign(m_numLayers, 0);
     sumThetaLayer.assign(m_numLayers, 0);
     sumWeightLayer.assign(m_numLayers, 0);
     // first check the energy deposited in each layer
     for (auto cell = newCluster.hits_begin(); cell != newCluster.hits_end(); cell++)
     {
       dd4hep::DDSegmentation::CellID cID = cell->getCellID();
-      uint layer = m_decoder[systemId]->get(cID, m_layerFieldName) + m_firstLayerId;
+      uint layer = m_decoder->get(cID, m_layerFieldName) + m_firstLayerId;
       sumEnLayer[layer] += cell->getEnergy();
     }
-    // sort energy to check value of 2nd highest, 3rd highest etc
-    for (uint iLayer = 0; iLayer < m_numLayers; iLayer++)
-    {
-      sumEnLayerSorted[iLayer] = sumEnLayer[iLayer];
-    }
-    std::sort(sumEnLayerSorted.begin(), sumEnLayerSorted.end(), std::greater<double>());
-    sumEnFirstLayer = sumEnLayer[0];
     // repeat but calculating theta barycentre in each layer
     for (auto cell = newCluster.hits_begin(); cell != newCluster.hits_end(); cell++)
     {
-      if (m_segmentationMulti[systemId] != nullptr)
-      {
-        segmentation = dynamic_cast<const dd4hep::DDSegmentation::FCCSWGridPhiTheta_k4geo *>(&m_segmentationMulti[systemId]->subsegmentation(cell->getCellID()));
-      }
+      // if (m_segmentationMulti != nullptr)
+      // {
+      //   segmentation = dynamic_cast<const dd4hep::DDSegmentation::FCCSWGridPhiTheta_k4geo *>(&m_segmentationMulti->subsegmentation(cell->getCellID()));
+      // }
       dd4hep::DDSegmentation::CellID cID = cell->getCellID();
-      uint layer = m_decoder[systemId]->get(cID, m_layerFieldName) + m_firstLayerId;
+      uint layer = m_decoder->get(cID, m_layerFieldName) + m_firstLayerId;
       double weightLog = std::max(0., m_thetaRecalcLayerWeights[layer] + log(cell->getEnergy() / sumEnLayer[layer]));
       double theta = segmentation->theta(cell->getCellID());
       sumThetaLayer[layer] += (weightLog * theta);
       sumWeightLayer[layer] += weightLog;
     }
     // calculate theta position weighting with energy deposited in layer
-    // this energy is a good estimator of 1/sigma^2 of (theta_barycentre-eta_MC) distribution
-    double layerWeight = 0;
-    double sumLayerWeight = 0;
-    double sumLayerWeight2point = 0;
-    double newTheta = 0;
-    double newThetaErrorRes = 0;
-    double newThetaErrorRes2point = 0;
-    for (uint iLayer = 0; iLayer < m_numLayers; iLayer++)
-    {
-      if (sumWeightLayer[iLayer] > 1e-10)
-      {
-        sumThetaLayer[iLayer] /= sumWeightLayer[iLayer];
-        newTheta += sumThetaLayer[iLayer] * sumEnLayer[iLayer];
-        // layerWeight = 1. / (pow(m_etaLayerResolutionSampling[iLayer], 2) / energy +  pow(m_etaLayerResolutionConst[iLayer], 2));
-        // sumLayerWeight += layerWeight;
-        // newThetaErrorRes += sumThetaLayer[iLayer] * layerWeight;
-        // if (iLayer == 1 || iLayer == 2) {
-        // newThetaErrorRes2point += sumThetaLayer[iLayer] * layerWeight;
-        // sumLayerWeight2point += layerWeight;
-        // }
-      }
-    }
-    newTheta /= energy;
-    // newThetaErrorRes /= sumLayerWeight;
-    // newThetaErrorRes2point /= sumLayerWeight2point;
-    // alter Cartesian position of a cluster using new theta position
-    double radius = pos.Perp();
-    double phi = pos.Phi();
-    auto newClusterPosition = edm4hep::Vector3f(radius * cos(phi), radius * sin(phi), radius * sinh(newTheta));
-    // newCluster.setPosition(newClusterPosition);
-    // NOT SAVING NEW POSITION FOR NOW
+    // this energy is a good estimator of 1/sigma^2 of (theta_barycentre-theta_MC) distribution
+    // double layerWeight = 0;
+    // double sumLayerWeight = 0;
+    // double sumLayerWeight2point = 0;
+    // double newTheta = 0;
+    // double newThetaErrorRes = 0;
+    // double newThetaErrorRes2point = 0;
+    // for (uint iLayer = 0; iLayer < m_numLayers; iLayer++)
+    // {
+    //   if (sumWeightLayer[iLayer] > 1e-10)
+    //   {
+    //     sumThetaLayer[iLayer] /= sumWeightLayer[iLayer];
+    //     newTheta += sumThetaLayer[iLayer] * sumEnLayer[iLayer];
+    //     // layerWeight = 1. / (pow(m_thetaLayerResolutionSampling[iLayer], 2) / energy +  pow(m_thetaLayerResolutionConst[iLayer], 2));
+    //     // sumLayerWeight += layerWeight;
+    //     // newThetaErrorRes += sumThetaLayer[iLayer] * layerWeight;
+    //     // if (iLayer == 1 || iLayer == 2) {
+    //     // newThetaErrorRes2point += sumThetaLayer[iLayer] * layerWeight;
+    //     // sumLayerWeight2point += layerWeight;
+    //     // }
+    //   }
+    // }
+    // newTheta /= energy;
+    // // newThetaErrorRes /= sumLayerWeight;
+    // // newThetaErrorRes2point /= sumLayerWeight2point;
+    // // alter Cartesian position of a cluster using new theta position
+    // double radius = pos.Perp();
+    // double phi = pos.Phi();
+    // auto newClusterPosition = edm4hep::Vector3f(radius * cos(phi), radius * sin(phi), radius * sinh(newTheta));
+    // // newCluster.setPosition(newClusterPosition);
+    // // NOT SAVING NEW POSITION FOR NOW
     newCluster.setPosition(cluster.getPosition());
   }
   return StatusCode::SUCCESS;
--- RecFCCeeCalorimeter/src/components/CorrectClusterBarycenters.h
+++ RecFCCeeCalorimeter/src/components/CorrectClusterBarycenters.h
@@ -3,9 +3,8 @@
 
 // Key4HEP
 #include "k4FWCore/DataHandle.h"
-class ITHistSvc;
 class IGeoSvc;
-class IRndmGenSvc;
+
 
 // Gaudi
 #include "GaudiAlg/GaudiAlgorithm.h"
@@ -15,7 +14,6 @@ class IRndmGenSvc;
 //EDM4HEP
 namespace edm4hep {
     class ClusterCollection;
-    // class CalorimeterHitCollection;
 }
 
 namespace dd4hep{
@@ -44,38 +42,34 @@ class CorrectClusterBarycenters : public GaudiAlgorithm{
     DataHandle<edm4hep::ClusterCollection> m_correctedClusters{"correctedClusters", Gaudi::DataHandle::Writer, this};
     /// Pointer to the geometry service
     ServiceHandle<IGeoSvc> m_geoSvc;
-    /// Random Number Service
-    IRndmGenSvc* m_randSvc;
-    /// Gaussian random number generator used for the generation of random noise hits
-    Rndm::Numbers m_gauss;
     /// Number of layers
     Gaudi::Property<uint> m_numLayers{this, "numLayers", 12, "Number of layers in the theta direction"};
     /// Weights for each detector layer for theta position log-weighting
     Gaudi::Property<std::vector<double>> m_thetaRecalcLayerWeights{
         this,
-        "etaRecalcWeights",
+        "ThetaRecalcWeights",
             {0, 3.0, 3.0, 3.0, 4.25, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0},
         "Weights for each detector layer for theta position log-weighting"};
-    Gaudi::Property<std::vector<double>> m_etaLayerResolutionSampling{
+    Gaudi::Property<std::vector<double>> m_thetaLayerResolutionSampling{
         this,
-        "etaLayerResolutionSampling",
+        "ThetaLayerResolutionSampling",
             {0, 1.405, 2.051, 3.195, 5.791, 10.28, 17.54, 30.5},
-        "sampling term of eta resolution"};
-    Gaudi::Property<std::vector<double>> m_etaLayerResolutionConst{
+        "sampling term of Theta resolution"};
+    Gaudi::Property<std::vector<double>> m_thetaLayerResolutionConst{
         this,
-        "etaLayerResolutionConst",
+        "ThetaLayerResolutionConst",
             {2.623, 0.1083, 0.1152, 0, 0, 0, 0, 0},
-        "const term of eta resolution"};
+        "const term of Theta resolution"};
     /// General decoder to encode the calorimeter sub-system to determine which positions tool to use
-    std::map<uint, dd4hep::DDSegmentation::BitFieldCoder*> m_decoder;
+    dd4hep::DDSegmentation::BitFieldCoder* m_decoder;
     /// IDs of the detectors
-    Gaudi::Property<std::vector<uint>> m_systemId{this, "systemId", {4}, "IDs of systems"};
+    Gaudi::Property<uint> m_systemId{this, "systemId", 4, "IDs of systems"};
     /// Names of the detector readout, corresponding to system IDs in m_systemId
-    Gaudi::Property<std::vector<std::string>> m_readoutName{
-        this, "readoutName", {"ECalBarrelModuleThetaMerged"}, "Names of the detector readout, corresponding to systemId"};
+    Gaudi::Property<std::string> m_readoutName{
+        this, "readoutName", "ECalBarrelModuleThetaMerged", "Names of the detector readout, corresponding to systemId"};
     /// map of system Id to segmentation, created based on m_readoutName and m_systemId
-    std::map<uint, dd4hep::DDSegmentation::FCCSWGridPhiTheta_k4geo*> m_segmentationPhiTheta;
-    std::map<uint, dd4hep::DDSegmentation::MultiSegmentation*> m_segmentationMulti;
+    dd4hep::DDSegmentation::FCCSWGridPhiTheta_k4geo* m_segmentationPhiTheta;
+    // dd4hep::DDSegmentation::MultiSegmentation* m_segmentationMulti;
     /// Name of the layer/cell field
     Gaudi::Property<std::string> m_layerFieldName{this, "layerFieldName", "layer", "Identifier of layers"};
     /// Id of the first layer
@@ -85,13 +79,11 @@ class CorrectClusterBarycenters : public GaudiAlgorithm{
     Gaudi::Property<std::vector<int>> m_nPhiFinal{this, "nPhiOptimFinal", {}};
     // Recalculate to half size N (window size = 2*N+1)
     std::vector<int> m_halfPhiFin;
-    /// Size of the window in eta for the final cluster building, optimised for each layer  (in units of cell size)
+    /// Size of the window in Theta for the final cluster building, optimised for each layer  (in units of cell size)
     /// If empty use same size for each layer, as in *nThetaFinal*
     Gaudi::Property<std::vector<int>> m_nThetaFinal{this, "nThetaOptimFinal", {}};
     // Recalculate to half size N (window size = 2*N+1)
     std::vector<int> m_halfThetaFin;
-    /// Flag for the ellipse used in the final cluster instead of the rectangle
-    Gaudi::Property<bool> m_ellipseFinalCluster{this, "ellipse", false};
 };
 
 #endif  /* RECFCCEECALORIMETER_CORRECTCLUSTERBARYCENTERS_H */
