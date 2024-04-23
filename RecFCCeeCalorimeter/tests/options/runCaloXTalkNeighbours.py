255127a703c3b623bdde479ddefdf68cf90ae452
Zhibo Wu <91186438+zwu0922@users.noreply.github.com>
2024-04-23 09:37:04 +0200

    Add crosstalk neighbour class (#69)
    * Add crosstalk neighbour class
    
    * Use c++ static_cast
    
    * Attempt to add automatic test
    
    * Adapt to the new headfile name of the k4geo update
    
    * revert neighbours to ExtSvc in the test
    
    * change function name: xtalk_get_cell_indices
    
    * Adapt to new k4geo function names for the crosstalk neighbours

---
+++ RecFCCeeCalorimeter/tests/options/runCaloXTalkNeighbours.py
@@ -0,0 +1,37 @@
+from Configurables import GeoSvc
+from Configurables import ApplicationMgr
+from Configurables import CreateFCCeeCaloXTalkNeighbours
+import os
+from Gaudi.Configuration import INFO, DEBUG
+
+# Detector geometry
+geoservice = GeoSvc("GeoSvc")
+# if K4GEO is empty, this should use relative path to working directory
+path_to_detector = os.environ.get("K4GEO", "")
+print(path_to_detector)
+detectors_to_use = [
+    'FCCee/ALLEGRO/compact/ALLEGRO_o1_v02/ALLEGRO_o1_v02.xml'
+]
+
+# prefix all xmls with path_to_detector
+geoservice.detectors = [os.path.join(path_to_detector, _det) for _det in detectors_to_use]
+geoservice.OutputLevel = INFO
+
+# create the crosstalk neighbour file for ECAL barrel cells
+neighbours = CreateFCCeeCaloXTalkNeighbours("xtalk_neighbours",
+                                       outputFileName="xtalk_neighbours_map_ecalB_thetamodulemerged.root",
+                                       readoutNames=["ECalBarrelModuleThetaMerged"],
+                                       systemNames=["system"],
+                                       systemValues=[4],
+                                       activeFieldNames=["layer"],
+                                       activeVolumesNumbers=[12],
+                                       OutputLevel=DEBUG)
+
+# ApplicationMgr
+ApplicationMgr(TopAlg=[],
+               EvtSel='NONE',
+               EvtMax=1,
+               # order is important, as GeoSvc is needed by G4SimSvc
+               ExtSvc=[geoservice, neighbours],
+               OutputLevel=INFO
+               )
