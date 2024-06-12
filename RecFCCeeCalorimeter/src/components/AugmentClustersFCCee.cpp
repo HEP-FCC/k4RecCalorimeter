14d9bf54815b2d79e3c1e024af11f161df944095
Giovanni Marchiori <giovanni.marchiori@cern.ch>
2024-06-12 23:11:28 +0200

    fix crash if input clusters do not have shapeParameters, and fix calibration code crash with new ONNX runtime

--- RecFCCeeCalorimeter/src/components/AugmentClustersFCCee.cpp
+++ RecFCCeeCalorimeter/src/components/AugmentClustersFCCee.cpp
@@ -67,8 +67,9 @@ StatusCode AugmentClustersFCCee::initialize()
   }
 
   // initialise the list of metadata for the clusters
-  // append to the metadata of the input clusters
-  std::vector<std::string> showerShapeDecorations = m_inShapeParameterHandle.get();
+  // append to the metadata of the input clusters (if any)
+  std::vector<std::string> emptyVector;
+  std::vector<std::string> showerShapeDecorations = m_inShapeParameterHandle.get(emptyVector);
   for (size_t k = 0; k < m_detectorNames.size(); k++)
   {
     const char *detector = m_detectorNames[k].c_str();
@@ -91,8 +92,6 @@ StatusCode AugmentClustersFCCee::finalize()
 
 StatusCode AugmentClustersFCCee::execute([[maybe_unused]] const EventContext &evtCtx) const
 {
-
-
   // get the input collection with clusters
   const edm4hep::ClusterCollection *inClusters = m_inClusters.get();
 
