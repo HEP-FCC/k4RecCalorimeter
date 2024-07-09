e252f8cff17cf8a032b774a98bc5bcf2d7a82459
Brieuc Francois <brieuc.francois@cern.ch>
2024-07-09 17:46:03 +0200

    Avoid crashing when cross-talk not wanted (#94)
    * [ReadCaloCrosstalkMap] Avoid crashing when cross talk file does not exists but we do not need cross talk emulation
    
    * [ReadCaloCrosstalkMap] Add a debug message when skipping initilization

--- RecCalorimeter/src/components/ReadCaloCrosstalkMap.cpp
+++ RecCalorimeter/src/components/ReadCaloCrosstalkMap.cpp
@@ -13,6 +13,14 @@ ReadCaloCrosstalkMap::ReadCaloCrosstalkMap(const std::string& type, const std::s
 }
 
 StatusCode ReadCaloCrosstalkMap::initialize() {
+  // prevent to initialize the tool if not intended (input file path empty)
+  // otherwise things will crash if m_fileName is not available
+  // not a perfect solution but tools seems to not be meant to be optional
+  if (m_fileName == "") {
+    debug() << "Empty 'fileName' provided, it means cross-talk map is not needed, exitting ReadCaloCrosstalkMap initilization" << endmsg;
+    return StatusCode::SUCCESS;
+  }
+
   StatusCode sc = GaudiTool::initialize();
   info() <<"Loading crosstalk map..." << endmsg;
   if (sc.isFailure()) return sc;
