150b1d376a3b9695cdfb387c71615c5c7beef2a9
Giovanni Marchiori <giovanni.marchiori@cern.ch>
2024-07-19 15:38:06 +0200

    make the code agnostic of detector IDs, remove unneeded dependency on specific segmentation class, improve naming of some methods

--- RecCalorimeter/src/components/ConstNoiseTool.cpp
+++ RecCalorimeter/src/components/ConstNoiseTool.cpp
@@ -17,29 +17,18 @@ ConstNoiseTool::ConstNoiseTool(const std::string& type, const std::string& name,
 }
 
 StatusCode ConstNoiseTool::initialize() {
-  // set for ECalBarrel system:5
-  // set for ECalEndcap system:6
-  // set for HCalEndcap system:7
-  // set for HCalBarrel system:8
-  // set for HCalExtBarrel system:9
-  // set for ECalFwd system:10
-  // set for HCalFwd system:11
 
-  m_systemNoiseConstMap.emplace(5, m_ECalBThreshold );
-  m_systemNoiseConstMap.emplace(6, m_ECalECThreshold  );
-  m_systemNoiseConstMap.emplace(7, m_HCalECThreshold );
-  m_systemNoiseConstMap.emplace(8, m_HCalBThreshold );
-  m_systemNoiseConstMap.emplace(9, m_HCalBThreshold );
-  m_systemNoiseConstMap.emplace(10, m_ECalFwdThreshold );
-  m_systemNoiseConstMap.emplace(11, m_HCalFwdThreshold );
-
-  m_systemNoiseOffsetMap.emplace(5, 0. );
-  m_systemNoiseOffsetMap.emplace(6, 0. );
-  m_systemNoiseOffsetMap.emplace(7, 0. );
-  m_systemNoiseOffsetMap.emplace(8, 0. );
-  m_systemNoiseOffsetMap.emplace(9, 0. );
-  m_systemNoiseOffsetMap.emplace(10,0. );
-  m_systemNoiseOffsetMap.emplace(11,0. );
+  // check that sizes of m_detectors and m_detectorNoiseRMS and m_detectorNoiseOffset are the same
+  if (m_detectorsNoiseRMS.size() != m_detectors.size())
+    {
+      error() << "Sizes of the detectors vector and detectorsNoiseRMS vector do not match, exiting!" << endmsg;
+      return StatusCode::FAILURE;
+    }
+  if (m_detectorsNoiseOffset.size() != m_detectors.size())
+    {
+      error() << "Sizes of the detectors vector and detectorsNoiseOffset vector do not match, exiting!" << endmsg;
+      return StatusCode::FAILURE;
+    }
 
   // Get GeoSvc
   m_geoSvc = service("GeoSvc");
@@ -49,6 +38,21 @@ StatusCode ConstNoiseTool::initialize() {
     return StatusCode::FAILURE;
   }
  
+  // loop over the detectors
+  for (size_t iDet=0; iDet<m_detectors.size(); iDet++) {
+    std::string detName = "DetID_" + m_detectors[iDet];
+    try {
+      int detID = m_geoSvc->getDetector()->constant<int>(detName);
+      m_systemNoiseRMSMap.emplace(detID, m_detectorsNoiseRMS[iDet]);
+      m_systemNoiseOffsetMap.emplace(detID, m_detectorsNoiseOffset[iDet]);
+      debug() << "Set noise RMS and offset for detector " << detName << " (ID=" << detID << ") to: "
+	      << m_detectorsNoiseRMS[iDet] << " , " << m_detectorsNoiseOffset[iDet] << endmsg;
+    } catch (const std::exception& e) {
+      error() << "Error: detector with name " << detName << " not found, exception raised: " << e.what() << endmsg;
+      return StatusCode::FAILURE;
+    }
+  }
+
   StatusCode sc = GaudiTool::initialize();
   if (sc.isFailure()) return sc;
 
@@ -60,30 +64,30 @@ StatusCode ConstNoiseTool::finalize() {
   return sc;
 }
 
-double ConstNoiseTool::getNoiseConstantPerCell(uint64_t aCellId) {
+double ConstNoiseTool::getNoiseRMSPerCell(uint64_t aCellId) {
 
-  double Noise = 0.;
+  double noiseRMS = 0.;
   // Get cells global coordinate "system"
   dd4hep::DDSegmentation::CellID cID = aCellId;
   unsigned cellSystem = m_decoder->get(cID, "system");
   // cell noise in system
-  if (m_systemNoiseConstMap.count(cellSystem))
-    Noise = m_systemNoiseConstMap[cellSystem];
+  if (m_systemNoiseRMSMap.count(cellSystem))
+    noiseRMS = m_systemNoiseRMSMap[cellSystem];
   else
-    warning() << "No noise constant set for subsystem " << cellSystem << "! Noise constant of cell set to 0. " << endmsg;
-  return Noise;
+    warning() << "No noise RMS set for subsystem " << cellSystem << "! Noise RMS of cell set to 0. " << endmsg;
+  return noiseRMS;
 }
 
 double ConstNoiseTool::getNoiseOffsetPerCell(uint64_t aCellId) {
 
-  double Noise = 0.;
+  double noiseOffset = 0.;
   // Get cells global coordinate "system"
   dd4hep::DDSegmentation::CellID cID = aCellId;
   unsigned cellSystem = m_decoder->get(cID, "system");
   // cell noise in system
   if (m_systemNoiseOffsetMap.count(cellSystem))
-    Noise = m_systemNoiseOffsetMap[cellSystem];
+    noiseOffset = m_systemNoiseOffsetMap[cellSystem];
   else
     warning() << "No noise offset set for subsystem " << cellSystem << "! Noise offset of cell set to 0. " << endmsg;
-  return Noise;
+  return noiseOffset;
 }
