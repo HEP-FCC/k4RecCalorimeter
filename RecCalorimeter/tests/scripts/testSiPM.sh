6df8bcb6cc6419c8319cd341598e9aeddf1ace83
Sanghyun Ko <sang.hyun.ko@cern.ch>
2025-03-27 21:52:20 +0900

    fix CTest failure

--- RecCalorimeter/tests/scripts/testSiPM.sh
+++ RecCalorimeter/tests/scripts/testSiPM.sh
@@ -6,6 +6,7 @@ git clone --no-checkout https://github.com/key4hep/k4geo
 cd k4geo
 
 git checkout HEAD FCCee/IDEA/compact/IDEA_o1_v03
+git checkout HEAD FCCee/MDI
 git checkout HEAD example
 
 cd $workdir
@@ -13,4 +14,3 @@ cd $workdir
 ddsim --steeringFile=k4geo/example/SteeringFile_IDEA_o1_v03.py --compactFile=k4geo/FCCee/IDEA/compact/IDEA_o1_v03/IDEA_o1_v03.xml -G --gun.distribution uniform --gun.particle e- --random.seed 1234567
 
 k4run RecCalorimeter/tests/options/runSiPMDualReadout.py
-
