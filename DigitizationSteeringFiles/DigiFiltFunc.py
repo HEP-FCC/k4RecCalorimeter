82264f676e962211da298105b007f87aa280227e
sasingh <sahibjeet.singh@cern.ch>
2025-08-14 20:55:06 +0200

    Update and clean up steering files

--- DigitizationSteeringFiles/DigiFiltFunc.py
+++ DigitizationSteeringFiles/DigiFiltFunc.py
@@ -1,5 +1,5 @@
 from Gaudi.Configuration import INFO
-from Configurables import CaloDigitizerFunc, CaloFilterFunc
+from Configurables import CaloDigitizerFunc, CaloFilterFunc, CaloAddNoise2Digits, CaloWhitening
 from k4FWCore import ApplicationMgr, IOSvc
 
 Nevts = 10                              # -1 means all events
@@ -11,8 +11,14 @@ ecalBarrelSignalShapePath = "SignalPulseShapes.root"  # path to the root file wi
 PulseShapeName = "Gaussian"             # name of the signal pulse shape
 GaussianMean = 100.0
 GaussianSigma = 20.0
-FilterSize = 7
+FilterSize = 5
 
+NumberOfNoiseSamplesToSimulate = 2000
+NoiseEnergy = 0.001 # 1 MeV +/- 0.1 MeV
+NoiseWidth = 0.0001
+NoiseSampleSimulationFName="NoiseInfoTest_New_Modded4InvCorr.root"
+
+WhiteningFilterName2Apply = "ZCA"
 
 io_svc = IOSvc()
 
@@ -29,33 +35,58 @@ io_svc.outputCommands = ["drop Lumi*",
                          "drop HCal*"]
 
 CaloDigitizer = CaloDigitizerFunc("CaloDigitizerFunc",
-                                signalFileName=ecalBarrelSignalShapePath,
-                                treename="Signal_shape",
-                                InputCollection=[ecalBarrelInputName],
-                                pulseInitTime = DigitInitTime,
-                                pulseEndTime = DigitEndTime,
-                                pulseSamplingLength = PulseSampleLen,
-                                pulseType = PulseShapeName,
-                                mu = GaussianMean,
-                                sigma = GaussianSigma,
-                                OutputCollection=["ECalBarrelDigitized"],)
+                                signalFileName=ecalBarrelSignalShapePath, # Path to a file that stores all cell IDs
+                                treename="Signal_shape", # Treename to access the cell IDs in the above file
+                                InputCollection=[ecalBarrelInputName], # Name of input collection
+                                pulseInitTime = DigitInitTime, # Time of pulse start [ns]
+                                pulseEndTime = DigitEndTime, # Time of pulse ending [ns]
+                                pulseSamplingLength = PulseSampleLen, # Number of samples in the signal pulse shape
+                                pulseType = PulseShapeName, # Name of the signal pulse shape
+                                mu = GaussianMean, # Mean of the Gaussian pulse shape
+                                sigma = GaussianSigma, # Sigma of the Gaussian pulse shape
+                                OutputCollection=["ECalBarrelDigitized"], # Name of output collection
+                                )
+
+CaloAddNoise = CaloAddNoise2Digits("CaloAddNoise",
+                                   InputCollection=["ECalBarrelDigitized"],
+                                   OutputCollection=["ECalBarrelDigitizedWithNoise"],
+                                   noiseEnergy=NoiseEnergy,
+                                   noiseWidth=NoiseWidth,
+                                   noiseSimSamples=NumberOfNoiseSamplesToSimulate,
+                                   noiseInfoFileName=NoiseSampleSimulationFName,
+                                   pulseSamplingLength = PulseSampleLen,
+                                   )
+
+CaloWhiteningFilter = CaloWhitening("CaloWhitening",
+                                   InputCollection=["ECalBarrelDigitizedWithNoise"],
+                                   OutputCollection=["ECalBarrelWhitenedDigits"],
+                                   noiseInfoFileName = NoiseSampleSimulationFName,
+                                   invCorrMatName = "InvertedCorrelationMatrix",
+                                   muVecName = "NoiseSampleMat_MeansVector",
+                                   filterName = WhiteningFilterName2Apply,
+                                   )
 
 CaloFilter = CaloFilterFunc("CaloFilterFunc",
-                            InputCollection = ["ECalBarrelDigitized"],
-                            OutputCollectionFilteredPulse = ["ECalBarrelMatchedFilterPulse"], 
-                            OutputCollectionMatchedSampleIdx = ["ECalBarrelMatchedFilterSampleIdx"],
-                            OutputCollectionMatchedSampleEnergy = ["ECalBarrelMatchedFilterSampleEnergy"],
+                            InputCollection = ["ECalBarrelWhitenedDigits"], # Name of input collection
+                            OutputCollectionFilteredPulse = ["ECalBarrelMatchedFilterPulse"], # Name of output collection
+                            OutputCollectionMatchedSampleIdx = ["ECalBarrelMatchedFilterSampleIdx"], # Name of output collection
+                            OutputCollectionMatchedSampleEnergy = ["ECalBarrelMatchedFilterSampleEnergy"], # Name of output collection
                             
-                            filterName = "Matched_%s" % PulseShapeName,
-                            pulseInitTime = DigitInitTime,
-                            pulseEndTime = DigitEndTime,
-                            pulseSamplingLength = PulseSampleLen,
-                            filterTemplateSize = FilterSize,
-                            mu = GaussianMean,
-                            sigma = GaussianSigma,
+                            filterName = "Matched_Gaussian", # Name of the filter template
+                            pulseInitTime = DigitInitTime, # Time of pulse start [ns]
+                            pulseEndTime = DigitEndTime, # Time of pulse ending [ns]
+                            pulseSamplingLength = PulseSampleLen, # Number of samples in the signal pulse shape
+                            filterTemplateSize = FilterSize, # Number of samples in the filter template
+                            mu = GaussianMean, # Mean of the Gaussian pulse shape
+                            sigma = GaussianSigma, # Sigma of the Gaussian pulse shape
+                            noiseInfoFileName = NoiseSampleSimulationFName,
+                            invCorrMatName = "InvertedCorrelationMatrix",
                             )
 
-ApplicationMgr(TopAlg=[CaloDigitizer, 
+ApplicationMgr(TopAlg=[
+                       CaloDigitizer, 
+                       CaloAddNoise,
+                       CaloWhiteningFilter,
                        CaloFilter,
                        ],
                EvtSel="NONE",
