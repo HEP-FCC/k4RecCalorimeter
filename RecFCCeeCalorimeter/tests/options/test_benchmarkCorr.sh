88ea013abd552d526bac006077ca9897c768f57e
Giovanni Marchiori <giovanni.marchiori@cern.ch>
2025-09-27 18:05:22 +0200

    run ddsim only once for ALLEGRO

--- RecFCCeeCalorimeter/tests/options/test_benchmarkCorr.sh
+++ RecFCCeeCalorimeter/tests/options/test_benchmarkCorr.sh
@@ -16,23 +16,6 @@ if [[ -z "${KEY4HEP_STACK}" ]]; then
   exit 1
 fi
 
-# run the SIM step (for debug do not run it if files already present. Comment the if and fi lines for production)
-if [ "$usePythia" -gt 0 ]; then
-  # download the events to reconstruct
-  if ! test -f ./pythia_ee_z_qq_10evt.hepmc; then
-    echo "Downloading files needed for simulation"
-    download_file "https://fccsw.web.cern.ch/fccsw/filesForSimDigiReco/gen/pythia_ee_z_qq_10evt.hepmc"
-  fi
-  # if ! test -f ALLEGRO_sim_ee_z_qq.root; then
-  echo "Performing the Geant4 simulation with ddsim"
-  ddsim --inputFiles pythia_ee_z_qq_10evt.hepmc --numberOfEvents 2 --outputFile ALLEGRO_sim_ee_z_qq.root --compactFile $K4GEO/FCCee/ALLEGRO/compact/ALLEGRO_o1_v03/ALLEGRO_o1_v03.xml || { retcode=$? ; echo "Simulation failed" ; exit $retcode ; }
-  # fi
-else
-  #if ! test -f ALLEGRO_sim_e_barrel.root; then
-  echo "Generating events and performing the Geant4 simulation with ddsim"
-  ddsim --enableGun --gun.distribution uniform --gun.energy "10*GeV" --gun.thetaMin "69*deg" --gun.thetaMax "69*deg" --gun.particle pi- --numberOfEvents 2 --outputFile benchmarkCorr_sim.root --random.enableEventSeed --random.seed 4242 --compactFile $K4GEO/FCCee/ALLEGRO/compact/ALLEGRO_o1_v03/ALLEGRO_o1_v03.xml || exit 1
-  #fi
-fi
 
 # get the files needed for calibration, noise, neighbor finding, etc
 if ! test -f ./cellNoise_map_endcapTurbine_electronicsNoiseLevel.root; then  # assumes that if the last file exists, all the other as well
@@ -56,7 +39,7 @@ fi
 # run the RECO step
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) # workaround to have ctests working
 if [ "$usePythia" -gt 0 ]; then
-  k4run $SCRIPT_DIR/test_benchmarkCorr.py
+  k4run $SCRIPT_DIR/test_benchmarkCorr.py --IOSvc.Input=ALLEGRO_sim_z_qq.root --IOSvc.Output=benchmarkCorr_rec.root || exit 1
 else
-  k4run $SCRIPT_DIR/test_benchmarkCorr.py --IOSvc.Input=benchmarkCorr_sim.root --IOSvc.Output=benchmarkCorr_rec.root || exit 1
+  k4run $SCRIPT_DIR/test_benchmarkCorr.py --IOSvc.Input=ALLEGRO_sim_pi.root --IOSvc.Output=benchmarkCorr_rec.root || exit 1
 fi
