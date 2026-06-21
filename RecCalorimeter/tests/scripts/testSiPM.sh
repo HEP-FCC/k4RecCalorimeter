940df1b108c98325b1c1c9a7169b71bf53a2316b
Giovanni Marchiori <giovanni.marchiori@cern.ch>
2026-06-22 00:13:16 +0200

    fix

--- RecCalorimeter/tests/scripts/testSiPM.sh
+++ RecCalorimeter/tests/scripts/testSiPM.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
 SOURCE_DIR=`dirname $0`
+workdir=$PWD
 
-ddsim --steeringFile=$K4GEO/example/SteeringFile_IDEA_o1_v03.py --compactFile=$K4GEO/FCCee/IDEA/compact/IDEA_o1_v03/IDEA_o1_v03.xml -G --gun.distribution uniform --gun.particle e- --random.seed 1234567 --numberOfEvents=5
+git clone --no-checkout https://github.com/key4hep/k4geo
+cd k4geo
+git checkout HEAD example
+cd $workdir
+
+ddsim --steeringFile=k4geo/example/SteeringFile_IDEA_o1_v03.py --compactFile=$K4GEO/FCCee/IDEA/compact/IDEA_o1_v03/IDEA_o1_v03.xml -G --gun.distribution uniform --gun.particle e- --random.seed 1234567 --numberOfEvents=5
 
 k4run $SOURCE_DIR/../options/runSiPMDualReadout.py
 
